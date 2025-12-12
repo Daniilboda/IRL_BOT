253b9648088c9006f2bce550f09e6300c72bd677
Daniilboda <bodukhind@mail.ru>
2025-12-12 07:48:37 +0300

    третье задание и исправление 2-го

--- routers/lesson_45.py
+++ routers/lesson_45.py
@@ -3,10 +3,11 @@ from aiogram.types import Message, CallbackQuery
 from .keyboards_45 import get_kb_45
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 import random
+import re
 import textwrap
 router = Router()
 
-
+# 1 здание
 TASK_45_1_TEXTS = [
     "— Как долго ... до станции?",
     "— Если вы ... через поле - 25 минут.",
@@ -26,6 +27,8 @@ TASK_45_1_CORRECT = {
     3 : "возьмёте, дойдёте",
 }
 
+
+#2 задание
 TASK_45_2_TEXTS = """
 Мы приехали в театр, когда спектакль уже начался. В [____] концерта музыкант исполнял концерт Чайковского.
 Через пять минут лекция закончится. В [____] лекции студенты обычно задают свои вопросы.
@@ -44,13 +47,30 @@ REMOVES_45_2 = {
 }
 
 
+# 3 задание
+TASK_45_3_TEXTS = ''' Виктор сидит за <b>(стол)</b> и читает газету. Вдруг он объявление о своей <b>(смерть)</b>.
 
+Виктор сразу позвонил своему <b>(друг)</b> Антону. — Антон! Ты читал объявление о <b>(моя смерть)</b>? — спросил Виктор.
+ — Да, — ответил Антон. — А откуда ты говоришь?'''
+
+TASK_45_3_TEXTS = [sent.strip() for sent in TASK_45_3_TEXTS.split('.')]
+# print(TASK_45_3_TEXTS)
+REPLACES_45 = ['столом', 'смерти', 'другу', 'моей смерти']
+TASK_45_3_CORRECT = ''' Виктор сидит за <b>столом</b> и читает газету. Вдруг он объявление о своей <b>смерти</b>.
+
+Виктор сразу позвонил своему <b>другу</b> Антону. — Антон! Ты читал объявление о <b>моей смерти</b>? — спросил Виктор.
+ — Да, — ответил Антон. — А откуда ты говоришь?'''
+
+#отправка клаиватуры 45 урока
 @router.message(F.text == "/lesson_45")
 async def lesson_45_start(message: Message):
     await message.answer(
         "Урок 45 — выберите задание:",
         reply_markup=get_kb_45()
     )
+
+
+# 4 задание
 
 
 current_index_45_1 = {}
@@ -66,7 +86,7 @@ current_buttons_45_4 = {}
 current_buttons_45_5 = {}
 current_buttons_45_6 = {}
 
-#функция отправки кнопок для 1-ой задачи
+#все функции для отправик примеров заданий
 async def send_45_1(message_or_callback, user_id):
     try:
         index = current_index_45_1[user_id]
@@ -98,10 +118,13 @@ async def send_45_1(message_or_callback, user_id):
         else:
             await lesson_45_start(message_or_callback)
         return
+
+
 SEND_TEXT_45_2 = ''
 async def send_45_2(message_or_callback, user_id):
     try:
         global SEND_TEXT_45_2
+        global TIP_45_2
         index = current_index_45_2.get(user_id, 0)
         print("send_45_2: user", user_id, "index", index)
         send_txt = textwrap.fill(SEND_TEXT_45_2 + TASK_45_2_TEXTS[index], width=60)
@@ -116,12 +139,46 @@ async def send_45_2(message_or_callback, user_id):
     except Exception:
         if isinstance(message_or_callback, CallbackQuery):
             await lesson_45_start(message_or_callback.message)
+            del current_index_45_2[user_id]
+            SEND_TEXT_45_2 = ''
+            TIP_45_2 = "продолжение,начало,исполнение,конец".split(',')
+
         else:
             await lesson_45_start(message_or_callback)
+            del current_index_45_2[user_id]
+            SEND_TEXT_45_2 = ''
+            TIP_45_2 = "продолжение,начало,исполнение,конец".split(',')
 
-
+SEND_TEXT_45_3 = ''
 async def send_45_3(message_or_callback, user_id):
-    pass
+    try:
+        global SEND_TEXT_45_3
+        index = current_index_45_3.get(user_id, 0)
+        if index >= len(REPLACES_45):  # если вышли за пределы текста
+            await message_or_callback.answer(textwrap.fill(TASK_45_3_CORRECT, width=60))
+            await lesson_45_start(message_or_callback)
+            del current_index_45_3[user_id]
+            SEND_TEXT_45_3 = ''
+            return
+        send_txt = textwrap.fill(SEND_TEXT_45_3 + TASK_45_3_TEXTS[index], width=60)
+        formatted_text = f"{send_txt}\n\n<b>Раскройте скобки:</b>"
+
+        if isinstance(message_or_callback, CallbackQuery):
+            await message_or_callback.message.answer(formatted_text)
+        else:
+            await message_or_callback.answer(formatted_text)
+    except Exception:
+        if isinstance(message_or_callback, CallbackQuery):
+            await message_or_callback.message.answer(textwrap.fill(TASK_45_3_CORRECT, width=60))
+            await lesson_45_start(message_or_callback.message)
+            del current_index_45_3[user_id]
+            SEND_TEXT_45_3 = ''
+        else:
+            await message_or_callback.answer(textwrap.fill(TASK_45_3_CORRECT, width=60))
+            await lesson_45_start(message_or_callback)
+            del current_index_45_3[user_id]
+            SEND_TEXT_45_3 = ''
+
 async def send_45_4(message_or_callback, user_id):
     pass
 async def send_45_5(message_or_callback, user_id):
@@ -175,9 +232,7 @@ async def handle_user_text(message: Message):
     if user_id in current_index_45_2:
         global SEND_TEXT_45_2
         reply = message.text.lower()
-        print(reply)
         index = current_index_45_2[user_id]
-        print(index)
         correct_word = TASK_45_2_CORRECT[index]
         current_sentence = TASK_45_2_TEXTS[index].replace('[____]', reply)
         right_sentence = TASK_45_2_TEXTS[index].replace('[____]', correct_word)
@@ -195,10 +250,38 @@ async def handle_user_text(message: Message):
         await send_45_2(message, user_id)
         return
 
+
+    #3 задание
+    elif user_id in current_index_45_3:
+        global SEND_TEXT_45_3
+        reply = message.text.lower()
+        # print(SEND_TEXT_45_3 +TASK_45_3_TEXTS[i])
+        index = current_index_45_3[user_id]
+
+        if index >= len(REPLACES_45):
+            await message.answer(textwrap.fill(TASK_45_3_CORRECT, width=60))
+            await lesson_45_start(message)
+            del current_index_45_3[user_id]
+            SEND_TEXT_45_3 = ''
+            return
+
+        if reply == REPLACES_45[index]:
+            await message.answer('✅ Верно!')
+            TASK_45_3_TEXTS[index] = re.sub(r'\(.*?\)', REPLACES_45[index], TASK_45_3_TEXTS[index])
+            SEND_TEXT_45_3+=TASK_45_3_TEXTS[index] + '. '
+        else:
+            await message.answer(f'❌ Неверно! Правильный ответ: {REPLACES_45[index]}')
+            TASK_45_3_TEXTS[index] = re.sub(r'\(.*?\)', REPLACES_45[index], TASK_45_3_TEXTS[index])
+            SEND_TEXT_45_3+=TASK_45_3_TEXTS[index] + '. '
+        current_index_45_3[user_id]+=1
+        await send_45_3(message, user_id)
+
+        print(SEND_TEXT_45_3 + TASK_45_3_TEXTS[-1])
+
     # # Если пользователь проходит задание 45.3
     # if user_id in current_index_45_3:
     #     await handle_task_45_3(message)
     #     return
 
     # Если он не в режиме теста
-    await message.answer("Напишите /lesson_45 чтобы начать.")
+    # await message.answer("Напишите /lesson_45 чтобы начать.")
